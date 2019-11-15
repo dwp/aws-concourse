c6a995b8a5273136f9a18ed37e2db6ffea546a64
Joe Stringer <joeds13@users.noreply.github.com>
2019-11-15 15:24:29 +0000

    Moving Concourse Workers to ASG (#12)
    fixes !5

---
+++ terraform/modules/concourse_worker/asg.tf
@@ -0,0 +1,67 @@
+resource "aws_autoscaling_group" "worker" {
+  name             = local.name
+  max_size         = var.worker.count
+  min_size         = var.worker.count
+  desired_capacity = var.worker.count
+
+  vpc_zone_identifier = var.vpc.aws_subnets_private[*].id
+
+  launch_template {
+    id      = aws_launch_template.worker.id
+    version = "$Latest"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_launch_template" "worker" {
+  name_prefix   = "${local.name}-"
+  image_id      = var.ami_id
+  instance_type = var.worker.instance_type
+  user_data     = data.template_cloudinit_config.worker_bootstrap.rendered
+  tags          = merge(var.tags, { Name = local.name })
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      delete_on_termination = true
+      encrypted             = true
+      volume_size           = 32
+    }
+  }
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    no_device   = true
+  }
+
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.worker.arn
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = merge(var.tags, { Name = local.name })
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+    tags          = merge(var.tags, { Name = local.name })
+  }
+
+  network_interfaces {
+    associate_public_ip_address = false
+    delete_on_termination       = true
+
+    security_groups = [
+      aws_security_group.worker.id
+    ]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
