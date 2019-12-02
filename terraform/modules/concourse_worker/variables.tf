903e367d851ce4a223a3f8eecc55385a57ba094b
Dragos Costache <dragos.costache@infinityworks.com>
2019-12-02 14:59:49 +0000

    Added aws_autoscaling_schedule s to concourse_worker asg

--- terraform/modules/concourse_worker/variables.tf
+++ terraform/modules/concourse_worker/variables.tf
@@ -28,3 +28,39 @@ variable "worker" {
     environment_override = {}
   }
 }
+
+variable "asg_night" {
+  description = "asg night schedule configuration"
+
+  type = object({
+    min_size = number
+    max_size = number
+    desired_capacity = number
+    time = string
+  })
+
+  default = {
+    min_size = 1
+    max_size = 1
+    desired_capacity = 1
+    time = "0 19 * * 1-5"
+  }
+}
+
+variable "asg_day" {
+  description = "asg day schedule configuration"
+
+  type = object({
+    min_size = number
+    max_size = number
+    desired_capacity = number
+    time = string
+  })
+
+  default = {
+    min_size = 3
+    max_size = 3
+    desired_capacity = 3
+    time = "0 7 * * 1-5"
+  }
+}
