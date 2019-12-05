b7ec556629b0b54c2530de6016e80425b8e542a2
Dragos Costache <dragos.costache@infinityworks.com>
2019-12-05 10:53:05 +0000

    Add cloudwatch agent to web

---
+++ terraform/modules/concourse_web/cloudwatch_agent.tf
@@ -0,0 +1,15 @@
+locals {
+  cloudwatch_agent_config_file = templatefile(
+    "${path.module}/templates/cloudwatch_agent_config.json",
+    {
+      cloudwatch_log_group : var.log_group.name
+    }
+  )
+
+}
+
+resource "aws_ssm_parameter" "cloudwatch_agent_config_web" {
+  name  = "/${var.ssm_name_prefix}/web-cloudwatch_agent_config"
+  type  = "String"
+  value = local.cloudwatch_agent_config_file
+}
