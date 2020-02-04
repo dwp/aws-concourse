04c61739209b4441c3f96f8bf8c5018e2a684e99
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2020-02-04 20:48:26 +0000

    apply tags to ASGs
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

--- terraform/modules/concourse_worker/asg.tf
+++ terraform/modules/concourse_worker/asg.tf
@@ -6,6 +6,16 @@ resource "aws_autoscaling_group" "worker" {
 
   vpc_zone_identifier = var.vpc.aws_subnets_private[*].id
 
+  dynamic "tag" {
+    for_each = var.tags
+
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
   launch_template {
     id      = aws_launch_template.worker.id
     version = "$Latest"
