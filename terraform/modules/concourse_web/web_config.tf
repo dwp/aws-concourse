56ecf849177a6187be9296db25f03e9389d4ef24
Dmitrijs Kovalenko <dmitrjis.kovalenko@gmail.com>
2020-02-04 12:28:10 +0000

    fmt

--- terraform/modules/concourse_web/web_config.tf
+++ terraform/modules/concourse_web/web_config.tf
@@ -27,14 +27,14 @@ locals {
       #CONCOURSE_PROMETHEUS_BIND_IP   = "0.0.0.0"
       #CONCOURSE_PROMETHEUS_BIND_PORT = 8081
 
-//      CONCOURSE_OIDC_DISPLAY_NAME  = var.cognito.name
-//      CONCOURSE_OIDC_CLIENT_ID     = data.aws_ssm_parameter.concourse_cognito_client_id.value
-//      CONCOURSE_OIDC_CLIENT_SECRET = data.aws_ssm_parameter.concourse_cognito_client_secret.value
-//      CONCOURSE_OIDC_ISSUER        = var.cognito.issuer
-//
-//      CONCOURSE_MAIN_TEAM_OIDC_GROUP = var.cognito.admin_group
-//      CONCOURSE_OIDC_GROUPS_KEY      = "cognito:groups"
-//      CONCOURSE_OIDC_USER_NAME_KEY   = "cognito:username"
+      //      CONCOURSE_OIDC_DISPLAY_NAME  = var.cognito.name
+      //      CONCOURSE_OIDC_CLIENT_ID     = data.aws_ssm_parameter.concourse_cognito_client_id.value
+      //      CONCOURSE_OIDC_CLIENT_SECRET = data.aws_ssm_parameter.concourse_cognito_client_secret.value
+      //      CONCOURSE_OIDC_ISSUER        = var.cognito.issuer
+      //
+      //      CONCOURSE_MAIN_TEAM_OIDC_GROUP = var.cognito.admin_group
+      //      CONCOURSE_OIDC_GROUPS_KEY      = "cognito:groups"
+      //      CONCOURSE_OIDC_USER_NAME_KEY   = "cognito:username"
 
       CONCOURSE_DATADOG_AGENT_HOST = "localhost"
       CONCOURSE_DATADOG_AGENT_PORT = 8125
