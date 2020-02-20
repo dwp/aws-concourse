39902a75f28f9acde631376a6b4ab7490ecf6f6d
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-02-20 14:34:15 +0000

    Pass through peering region

--- terraform/deploy/peering.tf
+++ terraform/deploy/peering.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc_peering_connection" "ucfs_github" {
   peer_owner_id = var.github_vpc.owner
   peer_vpc_id   = var.github_vpc.id
+  peer_region   = var.github_vpc.region
   vpc_id        = module.vpc.outputs.aws_vpc.id
 
   requester {
