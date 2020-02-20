996af25a799cb72d88ca6a1e27b1b9047f11b7e8
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-02-20 14:22:56 +0000

    DW-3346: Add VPC Peering connection to UCFS GitHub

---
+++ terraform/deploy/peering.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc_peering_connection" "ucfs_github" {
+  peer_owner_id = var.github_vpc.owner
+  peer_vpc_id   = var.github_vpc.id
+  vpc_id        = module.vpc.outputs.aws_vpc.id
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
