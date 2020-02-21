db3173ae0ff8e6b112ef34a44a2e048ce0b19cce
Matthew Burgess <549318+mattburgess@users.noreply.github.com>
2020-02-21 09:46:17 +0000

    Temporarily remove VPC peering connection options (#43)

--- terraform/deploy/peering.tf
+++ terraform/deploy/peering.tf
@@ -4,7 +4,8 @@ resource "aws_vpc_peering_connection" "ucfs_github" {
   peer_region   = var.github_vpc.region
   vpc_id        = module.vpc.outputs.aws_vpc.id
 
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
+  # Uncomment this when UCFS have accepted our connection request
+  # requester {
+  #   allow_remote_vpc_dns_resolution = true
+  # }
 }
