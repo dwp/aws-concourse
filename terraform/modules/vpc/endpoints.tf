809ba842a6cd6e76f487e69913f33c8a32ad362b
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-03-02 10:31:08 +0000

    Removed egress file, switched tack on subnet_id's

--- terraform/modules/vpc/endpoints.tf
+++ terraform/modules/vpc/endpoints.tf
@@ -1,12 +1,12 @@
 resource "aws_vpc_endpoint" "concourse" {
-  count             = local.zone_count
+  #   count             = local.zone_count
   vpc_id            = module.vpc.vpc.id
   service_name      = "aws.concourse.endpoint"
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [var.loadbalancer.security_group_id]
 
-  subnet_ids          = [aws_subnet.private[count.index].id]
+  subnet_ids          = ["aws_subnet.private.*.id"]
   private_dns_enabled = true
 }
 
