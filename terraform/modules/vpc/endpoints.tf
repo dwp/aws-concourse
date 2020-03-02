cb379d0b5f4e10ae152e354f8d4563492a1d46e4
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-03-02 11:41:46 +0000

    Removed service that belongs elsewhere

--- terraform/modules/vpc/endpoints.tf
+++ terraform/modules/vpc/endpoints.tf
@@ -10,8 +10,3 @@ resource "aws_vpc_endpoint" "concourse" {
   private_dns_enabled = true
 }
 
-
-resource "aws_vpc_endpoint_service" "example" {
-  acceptance_required        = false
-  network_load_balancer_arns = [var.loadbalancer.arn]
-}
