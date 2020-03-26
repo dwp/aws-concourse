c05a7e139c58cfea13e2d4f6c417244eb1403eda
Jim Barker <jim.barker@icsltd.co.uk>
2020-03-26 19:44:19 +0000

    temporary fudge fix for docker connectivity

--- terraform/modules/concourse_worker/security_groups.tf
+++ terraform/modules/concourse_worker/security_groups.tf
@@ -7,6 +7,17 @@ resource "aws_security_group" "worker" {
   }
 }
 
+#Currently required to get to docker hub. Need to fix this later.
+resource "aws_security_group_rule" "worker_outbound_https" {
+  description       = "worker outbound https connectivity"
+  from_port         = 443
+  protocol          = "all"
+  security_group_id = aws_security_group.worker.id
+  to_port           = 443
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "worker_lb_out_ssh" {
   description              = "outbound traffic to web nodes via lb"
   from_port                = local.service_port
