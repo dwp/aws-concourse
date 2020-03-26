f61c0175fe6cd41327d67ea59782d7300d1db202
Jim Barker <jim.barker@icsltd.co.uk>
2020-03-26 11:59:48 +0000

    tighten security group rules

--- terraform/modules/concourse_worker/security_groups.tf
+++ terraform/modules/concourse_worker/security_groups.tf
@@ -7,22 +7,12 @@ resource "aws_security_group" "worker" {
   }
 }
 
-resource "aws_security_group_rule" "worker_all_out" {
-  description       = "worker_all_out"
-  from_port         = 0
-  protocol          = "all"
-  security_group_id = aws_security_group.worker.id
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
 resource "aws_security_group_rule" "worker_lb_out_ssh" {
-  description       = "worker_lb_out_ssh"
-  from_port         = local.service_port
-  protocol          = "tcp"
-  security_group_id = aws_security_group.worker.id
-  to_port           = local.service_port
-  type              = "egress"
-  cidr_blocks       = var.vpc.aws_subnets_private[*].cidr_block
+  description              = "outbound traffic to web nodes via lb"
+  from_port                = local.service_port
+  protocol                 = "tcp"
+  security_group_id        = var.loadbalancer.security_group_id
+  to_port                  = local.service_port
+  type                     = "egress"
+  source_security_group_id = aws_security_group.worker.id
 }
