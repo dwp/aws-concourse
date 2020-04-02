bfa65788a5eeccc1e03b3f063d2c7ed2e206b000
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-04-02 16:14:16 +0100

    Pasta creep

--- terraform/deploy/peering.tf
+++ terraform/deploy/peering.tf
@@ -14,6 +14,6 @@ resource "aws_vpc_peering_connection" "ucfs_github" {
 resource "aws_route" "ucfs_github" {
   count                     = length(module.vpc.outputs.aws_route_table_private)
   route_table_id            = module.vpc.outputs.aws_route_table_private[count.index].id
-  destination_cidr_block    = var.github_vpc.cidr_block[count.index].cidr
+  destination_cidr_block    = var.github_vpc.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.ucfs_github.id
 }
