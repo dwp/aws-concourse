b6e4e1c0271da20dd6caf19c5333d7b4f3af1065
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-04-06 14:32:49 +0100

    Added SG for 2222
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

---
+++ terraform/modules/internal_loadbalancer/security_groups.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "internal_lb" {
+  vpc_id = var.vpc.aws_vpc.id
+  tags   = merge(var.tags, { Name = "${var.name}-lb" })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "internal_ssh_in" {
+  description       = "enable inbound connectivity from whitelisted endpoints"
+  from_port         = 2222
+  protocol          = "tcp"
+  security_group_id = aws_security_group.internal_lb.id
+  to_port           = 2222
+  type              = "ingress"
+  cidr_blocks       = var.whitelist_cidr_blocks
+}
