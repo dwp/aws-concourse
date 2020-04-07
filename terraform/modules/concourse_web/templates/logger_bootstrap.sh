2c2691af96a11f020cbfc579f8870d189b03d76b
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-04-07 14:01:38 +0100

    Fixed up logger script
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/modules/concourse_web/templates/logger_bootstrap.sh
+++ terraform/modules/concourse_web/templates/logger_bootstrap.sh
@@ -2,14 +2,13 @@
 
 set -euxo pipefail
 
-export https_proxy="${https_proxy}"
-curl -s -O https://s3.amazonaws.com/amazoncloudwatch-agent/assets/amazon-cloudwatch-agent.gpg
-curl -s -O https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm.sig
-curl -s -O https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-unset https_proxy
+REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
+curl -s -O https://s3.$REGION.amazonaws.com/amazoncloudwatch-agent-$REGION/assets/amazon-cloudwatch-agent.gpg
+curl -s -O https://s3.$REGION.amazonaws.com/amazoncloudwatch-agent-$REGION/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm.sig
+curl -s -O https://s3.$REGION.amazonaws.com/amazoncloudwatch-agent-$REGION/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 
 gpg --import amazon-cloudwatch-agent.gpg
-gpg --verify cloudwatch.rpm.sig cloudwatch.rpm
-rpm -U ./cloudwatch.rpm
+gpg --verify amazon-cloudwatch-agent.rpm.sig amazon-cloudwatch-agent.rpm
+rpm -U ./amazon-cloudwatch-agent.rpm
 
-sudo /opt/aws/awsagent/bin/awsagent -a fetch-config -m ec2 -c ssm:${cloudwatch_agent_config_ssm_parameter} -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:${cloudwatch_agent_config_ssm_parameter} -s
