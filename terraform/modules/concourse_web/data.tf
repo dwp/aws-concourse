b7035c4be92c74da7dc2344cbc93678211de11a3
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-04-14 14:47:19 +0100

    Exposed SSM values for cognito
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/modules/concourse_web/data.tf
+++ terraform/modules/concourse_web/data.tf
@@ -18,10 +18,10 @@ data "aws_ssm_parameter" "concourse_password" {
   name = var.concourse_secrets.password_ssm_name
 }
 
-//data "aws_ssm_parameter" "concourse_cognito_client_id" {
-//  name = var.cognito.client_id_ssm_parameter
-//}
-//
-//data "aws_ssm_parameter" "concourse_cognito_client_secret" {
-//  name = var.cognito.client_secret_ssm_parameter
-//}
+data "aws_ssm_parameter" "concourse_cognito_client_id" {
+  name = var.cognito.client_id_ssm_parameter
+}
+
+data "aws_ssm_parameter" "concourse_cognito_client_secret" {
+  name = var.cognito.client_secret_ssm_parameter
+}
