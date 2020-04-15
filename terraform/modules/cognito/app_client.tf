870c2307d57b10906017f92504abb740a73edfd8
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-04-16 00:35:40 +0100

    DW-3420: Fix Concourse-Cognito integration
    When trying to login to Concourse via the Cognito integration, I was seeing a
    `invalid_scope` error. It turns out that the `profile` scope is required and
    the `aws.cognito.signin.user.admin` isn't; at least for the basic login flow
    to work.
    
    Also, remove the `explicit_auth_flows` as they're classed as optional and
    omitting them doesn't seem to break anything...yet.

--- terraform/modules/cognito/app_client.tf
+++ terraform/modules/cognito/app_client.tf
@@ -4,10 +4,8 @@ resource aws_cognito_user_pool_client app_client {
   user_pool_id                         = aws_cognito_user_pool.concourse.id
   generate_secret                      = true
   allowed_oauth_flows_user_pool_client = true
-
-  callback_urls = formatlist("https://%s/sky/issuer/callback", var.loadbalancer.fqdn)
-
-  supported_identity_providers = ["COGNITO"]
+  callback_urls                        = formatlist("https://%s/sky/issuer/callback", var.loadbalancer.fqdn)
+  supported_identity_providers         = ["COGNITO"]
 
   allowed_oauth_flows = [
     "code",
@@ -17,12 +15,7 @@ resource aws_cognito_user_pool_client app_client {
   allowed_oauth_scopes = [
     "email",
     "openid",
-    "aws.cognito.signin.user.admin",
+    "profile",
   ]
 
-  explicit_auth_flows = [
-    "ALLOW_CUSTOM_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH",
-    "ALLOW_USER_SRP_AUTH",
-  ]
 }
