a26f8578bd154696fc82d1b1b6c9b9dd7e4bc57f
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-04-28 16:27:03 +0100

    DW-3874: Increase request size limit to 128k and add WAF rule justification

--- terraform/modules/waf/waf_geo_match.tf
+++ terraform/modules/waf/waf_geo_match.tf
@@ -1,11 +1,13 @@
 resource "aws_wafregional_geo_match_set" "gb" {
   name = "geo_match_set"
 
+  ## UCFS traffic originates from GB
   geo_match_constraint {
     type  = "Country"
     value = "GB"
   }
 
+  ## github.com (for webhook traffic) is located in the US
   geo_match_constraint {
     type  = "Country"
     value = "US"
