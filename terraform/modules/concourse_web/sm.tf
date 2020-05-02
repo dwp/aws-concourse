08a575a7c7a1294d69771ade0a9680c4bbbe5b98
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-02 07:06:22 +0100

    Removed #
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/modules/concourse_web/sm.tf
+++ terraform/modules/concourse_web/sm.tf
@@ -1,5 +1,4 @@
 data "aws_secretsmanager_secret" "concourse-github-auth" {
-  # arn = "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:/concourse/main/binary"
   name = "/concourse/dataworks/dataworks-secret"
 }
 
