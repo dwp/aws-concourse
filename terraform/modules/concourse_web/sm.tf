871fb156050d2df34f7abd97eeb9238996c135e1
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-05-05 14:22:09 +0100

    Needs own auth keys
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/modules/concourse_web/sm.tf
+++ terraform/modules/concourse_web/sm.tf
@@ -5,3 +5,11 @@ data "aws_secretsmanager_secret" "concourse-github-auth" {
 data "aws_secretsmanager_secret_version" "concourse-github-auth" {
   secret_id = data.aws_secretsmanager_secret.concourse-github-auth.id
 }
+
+data "aws_secretsmanager_secret" "concourse-secrets" {
+  name = "/concourse/dataworks/dataworks"
+}
+
+data "aws_secretsmanager_secret_version" "concourse-secrets" {
+  secret_id = data.aws_secretsmanager_secret.concourse-secrets.id
+}
