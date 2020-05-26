c3a5b9304624b2b2af4da650d653e0f34467bc2d
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-05-26 13:15:08 +0100

    Adding reboot worker nodes for mgmt dev

--- bootstrap_ci_pipeline.py
+++ bootstrap_ci_pipeline.py
@@ -46,6 +46,10 @@ def main():
         template = jinja2.Template(in_template.read())
     with open('ci/jobs/reboot-worker-management.yml', 'w+') as pipeline:
         pipeline.write(template.render(config_data))
+    with open('ci/jobs/reboot-worker-management-dev.yml.j2') as in_template:
+        template = jinja2.Template(in_template.read())
+    with open('ci/jobs/reboot-worker-management-dev.yml', 'w+') as pipeline:
+        pipeline.write(template.render(config_data))
     print("Concourse pipeline config successfully created")
 
 
