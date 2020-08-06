2f3d9ad4b8d6d54108f13995f6b89442731eb503
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-08-06 15:59:51 +0100

    First push
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -8,8 +8,9 @@ output "outputs" {
     aws_vpc                 = module.vpc.vpc
     internet_egress_sg      = aws_security_group.internet_proxy_endpoint
     internet_proxy_endpoint = aws_vpc_endpoint.internet_proxy.dns_entry[0].dns_name
-    s3_prefix_list_id       = module.vpc.s3_prefix_list_id
-    dynamodb_prefix_list_id = module.vpc.dynamodb_prefix_list_id
+    s3_prefix_list_id       = module.vpc.prefix_list_ids.s3
+    dynamodb_prefix_list_id = module.vpc.prefix_list_ids.dynamodb
     route_tables_private    = aws_route_table.private
+    no_proxy_list           = module.vpc.no_proxy_list
   }
 }
--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,33 +1,37 @@
 module "vpc" {
-  source                                     = "dwp/vpc/aws"
-  version                                    = "2.0.14"
-  vpc_name                                   = "ci-cd"
-  region                                     = data.aws_region.current.name
-  vpc_cidr_block                             = var.vpc_cidr_block
-  interface_vpce_source_security_group_count = length(var.vpc_endpoint_source_sg_ids)
-  interface_vpce_source_security_group_ids   = var.vpc_endpoint_source_sg_ids
-  interface_vpce_subnet_ids                  = aws_subnet.private.*.id
-  gateway_vpce_route_table_ids               = aws_route_table.private.*.id
-  ec2autoscaling_endpoint                    = true
-  ec2_endpoint                               = true
-  ec2messages_endpoint                       = true
-  kms_endpoint                               = true
-  logs_endpoint                              = true
-  monitoring_endpoint                        = true
-  s3_endpoint                                = true
-  ssm_endpoint                               = true
-  ssmmessages_endpoint                       = true
-  secretsmanager_endpoint                    = true
-  ecrapi_endpoint                            = true
-  ecrdkr_endpoint                            = true
-  ecs_endpoint                               = true
-  elasticloadbalancing_endpoint              = true
-  events_endpoint                            = true
-  application_autoscaling_endpoint           = true
-  kinesis_firehose_endpoint                  = true
-  glue_endpoint                              = true
-  emr_endpoint                               = true
-  dynamodb_endpoint                          = true
-  efs_endpoint                               = true
-  common_tags                                = merge(var.tags, { Name = var.name })
+  source                                   = "dwp/vpc/aws"
+  version                                  = "3.0.5"
+  vpc_name                                 = "ci-cd"
+  region                                   = data.aws_region.current.name
+  vpc_cidr_block                           = var.vpc_cidr_block
+  interface_vpce_source_security_group_ids = var.vpc_endpoint_source_sg_ids
+  interface_vpce_subnet_ids                = aws_subnet.private.*.id
+  gateway_vpce_route_table_ids             = aws_route_table.private.*.id
+
+  aws_vpce_services = [
+    "ec2autoscaling",
+    "ec2",
+    "ec2messages",
+    "kms",
+    "logs",
+    "monitoring",
+    "s3",
+    "ssm",
+    "ssmmessages",
+    "secretsmanager",
+    "ecrapi",
+    "ecr.dkr",
+    "ecs",
+    "elasticloadbalancing",
+    "events",
+    "application_autoscaling",
+    "kinesis_firehose",
+    "glue",
+    "emr",
+    "dynamodb",
+    "efs",
+    "sns",
+    "athena"
+  ]
+  common_tags = merge(var.tags, { Name = var.name })
 }
