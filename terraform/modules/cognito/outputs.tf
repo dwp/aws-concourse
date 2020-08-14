921de9ff4eb8d9e49463f188156b18485a7e5f80
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-08-14 14:45:16 +0100

    Only required outputs
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -1,11 +1,9 @@
 output outputs {
   value = {
-    name                = aws_cognito_user_pool_client.app_client.name
-    app_client          = aws_cognito_user_pool_client.app_client
-    user_pool_domain    = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
-    user_pool_id        = aws_cognito_user_pool.concourse.id
-    user_pool_arn       = aws_cognito_user_pool.concourse.arn
-    user_pool_client_id = aws_cognito_user_pool_client.app_client.id
-    issuer              = "https://cognito-idp.${local.region}.amazonaws.com/${aws_cognito_user_pool.concourse.id}"
+    name             = aws_cognito_user_pool_client.app_client.name
+    app_client       = aws_cognito_user_pool_client.app_client
+    user_pool_domain = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
+    user_pool        = aws_cognito_user_pool.concourse
+    issuer           = "https://cognito-idp.${local.region}.amazonaws.com/${aws_cognito_user_pool.concourse.id}"
   }
 }
