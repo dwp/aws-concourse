e8f3437f36574f1d57637cfe0b2ef42e56cc3a67
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-08-14 13:57:49 +0100

    DW-4690 - New Cognito outputs
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -1,9 +1,11 @@
 output outputs {
   value = {
-    name             = aws_cognito_user_pool_client.app_client.name
-    app_client       = aws_cognito_user_pool_client.app_client
-    user_pool_domain = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
-    user_pool_id     = aws_cognito_user_pool.concourse.id
-    issuer           = "https://cognito-idp.${local.region}.amazonaws.com/${aws_cognito_user_pool.concourse.id}"
+    name                = aws_cognito_user_pool_client.app_client.name
+    app_client          = aws_cognito_user_pool_client.app_client
+    user_pool_domain    = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
+    user_pool_id        = aws_cognito_user_pool.concourse.id
+    user_pool_arn       = aws_cognito_user_pool.concourse.arn
+    user_pool_client_id = aws_cognito_user_pool_client.app_client.id
+    issuer              = "https://cognito-idp.${local.region}.amazonaws.com/${aws_cognito_user_pool.concourse.id}"
   }
 }
