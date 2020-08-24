064a7e2d2f144a155b0e85c46668ba7495cf2e1a
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-08-24 10:11:03 +0100

    DW-4690 - Fix duplicated output name
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -1,10 +1,10 @@
 output outputs {
   value = {
-    name             = aws_cognito_user_pool_client.app_client.name
-    app_client       = aws_cognito_user_pool_client.app_client
-    user_pool_domain = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
-    user_pool        = aws_cognito_user_pool.concourse
-    user_pool_domain = aws_cognito_user_pool_domain.main
-    issuer           = "https://cognito-idp.${local.region}.amazonaws.com/${aws_cognito_user_pool.concourse.id}"
+    name                  = aws_cognito_user_pool_client.app_client.name
+    app_client            = aws_cognito_user_pool_client.app_client
+    user_pool_domain      = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${local.region}.amazoncognito.com"
+    user_pool             = aws_cognito_user_pool.concourse
+    user_pool_main_domain = aws_cognito_user_pool_domain.main
+    issuer                = "https://cognito-idp.${local.region}.amazonaws.com/${aws_cognito_user_pool.concourse.id}"
   }
 }
