87445e17befc29f4abb8ed25915244f17b60f926
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-08-25 10:11:24 +0100

    bootstrap via role
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -9,24 +9,36 @@ import yaml
 
 
 def main():
+    region_name = os.environ.get('AWS_DEFAULT_REGION')
     if 'AWS_PROFILE' in os.environ:
-        boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'])
-    if 'AWS_REGION' in os.environ:
-        ssm = boto3.client('ssm', region_name=os.environ['AWS_REGION'])
+        session = boto3.Session(profile_name=os.environ['AWS_PROFILE'])
+    elif 'AWS_ROLE_ARN' in os.environ:
+        sts = boto3.client('sts')
+        response = sts.assume_role(
+            RoleArn=os.environ['AWS_ROLE_ARN'],
+            RoleSessionName='Bootstrap')
+        session = boto3.Session(
+            aws_access_key_id=response['Credentials']['AccessKeyId'],
+            aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+            aws_session_token=response['Credentials']['SessionToken'])
     else:
-        ssm = boto3.client('ssm')
+        session = boto3.Session()
+    ssm = boto3.client('ssm', region_name=region_name)
 
     try:
-        parameter = ssm.get_parameter(Name='terraform_bootstrap_config', WithDecryption=False)
+        parameter = ssm.get_parameter(
+            Name='terraform_bootstrap_config', WithDecryption=False)
     except botocore.exceptions.ClientError as e:
         error_message = e.response["Error"]["Message"]
         if "The security token included in the request is invalid" in error_message:
-            print("ERROR: Invalid security token used when calling AWS SSM. Have you run `aws-sts` recently?")
+            print(
+                "ERROR: Invalid security token used when calling AWS SSM. Have you run `aws-sts` recently?")
         else:
             print("ERROR: Problem calling AWS SSM: {}".format(error_message))
         sys.exit(1)
 
-    config_data = yaml.load(parameter['Parameter']['Value'], Loader=yaml.FullLoader)
+    config_data = yaml.load(
+        parameter['Parameter']['Value'], Loader=yaml.FullLoader)
     with open('terraform/deploy/terraform.tf.j2') as in_template:
         template = jinja2.Template(in_template.read())
     with open('terraform/deploy/terraform.tf', 'w+') as terraform_tf:
@@ -40,4 +52,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
