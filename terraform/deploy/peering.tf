3a5422863c85df159b74b7fc9a5b76a11005ead6
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-09-11 16:02:05 +0100

    peer_region cannot be set whilst auto_accept is true when creating a vpc peering connection
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/deploy/peering.tf
+++ terraform/deploy/peering.tf
@@ -1,7 +1,6 @@
 resource "aws_vpc_peering_connection" "ucfs_github" {
   peer_owner_id = var.github_vpc.owner
   peer_vpc_id   = var.github_vpc.id
-  peer_region   = var.github_vpc.region
   vpc_id        = module.vpc.outputs.aws_vpc.id
   auto_accept   = true
 
