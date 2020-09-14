d947947dc53c9377c78dce13a32d13c41f293481
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-09-14 10:28:15 +0100

    restored peering connection to previous config
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/deploy/peering.tf
+++ terraform/deploy/peering.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc_peering_connection" "ucfs_github" {
   peer_owner_id = var.github_vpc.owner
   peer_vpc_id   = var.github_vpc.id
+  peer_region   = var.github_vpc.region
   vpc_id        = module.vpc.outputs.aws_vpc.id
-  auto_accept   = true
 
   requester {
     allow_classic_link_to_remote_vpc = false
