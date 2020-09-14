749c59efaa982fa74134a0cc33322caa5d6a0ad7
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-09-14 13:29:45 +0100

    made master-like, removing older customisations
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -12,24 +12,28 @@ from dateutil.tz import tzlocal
 
 
 def main():
-    region_name = os.environ.get('AWS_DEFAULT_REGION')
     if 'AWS_PROFILE' in os.environ:
         boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'])
     if 'AWS_PROFILE_MGT_DEV' in os.environ:
-        secrets_session = boto3.Session(profile_name=os.environ['AWS_PROFILE_MGT_DEV'])
+        secrets_session = boto3.Session(
+            profile_name=os.environ['AWS_PROFILE_MGT_DEV'])
     elif 'AWS_SECRETS_ROLE' in os.environ:
         secrets_session = assumed_role_session(os.environ['AWS_SECRETS_ROLE'])
     if 'AWS_REGION' in os.environ:
         ssm = boto3.client('ssm', region_name=os.environ['AWS_REGION'])
-        secrets_manager = secrets_session.client('secretsmanager', region_name=os.environ['AWS_REGION'])
+        secrets_manager = secrets_session.client(
+            'secretsmanager', region_name=os.environ['AWS_REGION'])
     else:
         ssm = boto3.client('ssm')
         secrets_manager = secrets_session.client('secretsmanager')
 
     try:
-        parameter = ssm.get_parameter(Name='terraform_bootstrap_config', WithDecryption=False)
-        dataworks_secret = secrets_manager.get_secret_value(SecretId="/concourse/dataworks/dataworks-secrets")
-        concourse_secret = secrets_manager.get_secret_value(SecretId="/concourse/dataworks/dataworks")
+        parameter = ssm.get_parameter(
+            Name='terraform_bootstrap_config', WithDecryption=False)
+        dataworks_secret = secrets_manager.get_secret_value(
+            SecretId="/concourse/dataworks/dataworks-secrets")
+        concourse_secret = secrets_manager.get_secret_value(
+            SecretId="/concourse/dataworks/dataworks")
     except botocore.exceptions.ClientError as e:
         error_message = e.response["Error"]["Message"]
         if "The security token included in the request is invalid" in error_message:
@@ -39,22 +43,36 @@ def main():
             print("ERROR: Problem calling AWS SSM: {}".format(error_message))
         sys.exit(1)
 
-    config_data = yaml.load(parameter['Parameter']['Value'], Loader=yaml.FullLoader)
-    config_data['concourse_username'] = json.loads(dataworks_secret['SecretBinary'])["concourse_user"]
-    config_data['concourse_password'] = json.loads(dataworks_secret['SecretBinary'])["concourse_password"]
-    config_data['database_username'] = json.loads(dataworks_secret['SecretBinary'])["database_user"]
-    config_data['database_password'] = json.loads(dataworks_secret['SecretBinary'])["database_password"]
-    config_data['enterprise_github_oauth_client_id'] = json.loads(dataworks_secret['SecretBinary'])["enterprise_github_oauth_client_id"]
-    config_data['enterprise_github_oauth_client_secret'] = json.loads(dataworks_secret['SecretBinary'])["enterprise_github_oauth_client_secret"]
-    config_data['enterprise_github_url'] = json.loads(concourse_secret['SecretBinary'])["enterprise_github_url"]
-    config_data['session_signing_key'] = json.loads(dataworks_secret['SecretBinary'])["session_signing_key"]
-    config_data['session_signing_pub_key'] = json.loads(dataworks_secret['SecretBinary'])["session_signing_pub_key"]
-    config_data['tsa_host_key'] = json.loads(dataworks_secret['SecretBinary'])["tsa_host_key"]
-    config_data['tsa_host_pub_key'] = json.loads(dataworks_secret['SecretBinary'])["tsa_host_pub_key"]
-    config_data['worker_key'] = json.loads(dataworks_secret['SecretBinary'])["worker_key"]
-    config_data['worker_pub_key'] = json.loads(dataworks_secret['SecretBinary'])["worker_pub_key"]
-    config_data['authorized_worker_keys'] = json.loads(dataworks_secret['SecretBinary'])["authorized_worker_keys"]
-
+    config_data = yaml.load(
+        parameter['Parameter']['Value'], Loader=yaml.FullLoader)
+    config_data['concourse_username'] = json.loads(
+        dataworks_secret['SecretBinary'])["concourse_user"]
+    config_data['concourse_password'] = json.loads(
+        dataworks_secret['SecretBinary'])["concourse_password"]
+    config_data['database_username'] = json.loads(
+        dataworks_secret['SecretBinary'])["database_user"]
+    config_data['database_password'] = json.loads(
+        dataworks_secret['SecretBinary'])["database_password"]
+    config_data['enterprise_github_oauth_client_id'] = json.loads(
+        dataworks_secret['SecretBinary'])["enterprise_github_oauth_client_id"]
+    config_data['enterprise_github_oauth_client_secret'] = json.loads(
+        dataworks_secret['SecretBinary'])["enterprise_github_oauth_client_secret"]
+    config_data['enterprise_github_url'] = json.loads(
+        concourse_secret['SecretBinary'])["enterprise_github_url"]
+    config_data['session_signing_key'] = json.loads(
+        dataworks_secret['SecretBinary'])["session_signing_key"]
+    config_data['session_signing_pub_key'] = json.loads(
+        dataworks_secret['SecretBinary'])["session_signing_pub_key"]
+    config_data['tsa_host_key'] = json.loads(
+        dataworks_secret['SecretBinary'])["tsa_host_key"]
+    config_data['tsa_host_pub_key'] = json.loads(
+        dataworks_secret['SecretBinary'])["tsa_host_pub_key"]
+    config_data['worker_key'] = json.loads(
+        dataworks_secret['SecretBinary'])["worker_key"]
+    config_data['worker_pub_key'] = json.loads(
+        dataworks_secret['SecretBinary'])["worker_pub_key"]
+    config_data['authorized_worker_keys'] = json.loads(
+        dataworks_secret['SecretBinary'])["authorized_worker_keys"]
     with open('terraform/deploy/terraform.tf.j2') as in_template:
         template = jinja2.Template(in_template.read())
     with open('terraform/deploy/terraform.tf', 'w+') as terraform_tf:
@@ -69,21 +87,22 @@ def main():
 def assumed_role_session(role_arn: str, base_session: botocore.session.Session = None):
     base_session = base_session or boto3.session.Session()._session
     fetcher = botocore.credentials.AssumeRoleCredentialFetcher(
-        client_creator = base_session.create_client,
-        source_credentials = base_session.get_credentials(),
-        role_arn = role_arn,
-        extra_args = {
+        client_creator=base_session.create_client,
+        source_credentials=base_session.get_credentials(),
+        role_arn=role_arn,
+        extra_args={
             #    'RoleSessionName': None # set this if you want something non-default
         }
     )
     creds = botocore.credentials.DeferredRefreshableCredentials(
-        method = 'assume-role',
-        refresh_using = fetcher.fetch_credentials,
-        time_fetcher = lambda: datetime.datetime.now(tzlocal())
+        method='assume-role',
+        refresh_using=fetcher.fetch_credentials,
+        time_fetcher=lambda: datetime.datetime.now(tzlocal())
     )
     botocore_session = botocore.session.Session()
     botocore_session._credentials = creds
-    return boto3.Session(botocore_session = botocore_session)
+    return boto3.Session(botocore_session=botocore_session)
+
 
 if __name__ == "__main__":
     main()
