958ed760ef3bafaea3efaa3922a1eed61af64c1b
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-09-15 10:27:14 +0100

    Need to leave this hashed until uc do their side
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/deploy/peering.tf
+++ terraform/deploy/peering.tf
@@ -4,20 +4,20 @@ resource "aws_vpc_peering_connection" "ucfs_github" {
   peer_region   = var.github_vpc.region
   vpc_id        = module.vpc.outputs.aws_vpc.id
 
-  requester {
-    allow_classic_link_to_remote_vpc = false
-    allow_remote_vpc_dns_resolution  = true
-    allow_vpc_to_remote_classic_link = false
-  }
+  # requester {
+  #   allow_classic_link_to_remote_vpc = false
+  #   allow_remote_vpc_dns_resolution  = true
+  #   allow_vpc_to_remote_classic_link = false
+  # }
 
   lifecycle {
     prevent_destroy = true
   }
 }
 
-resource "aws_route" "ucfs_github" {
-  count                     = length(module.vpc.outputs.aws_route_table_private)
-  route_table_id            = module.vpc.outputs.aws_route_table_private[count.index].id
-  destination_cidr_block    = var.github_vpc.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.ucfs_github.id
-}
+# resource "aws_route" "ucfs_github" {
+#   count                     = length(module.vpc.outputs.aws_route_table_private)
+#   route_table_id            = module.vpc.outputs.aws_route_table_private[count.index].id
+#   destination_cidr_block    = var.github_vpc.cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.ucfs_github.id
+# }
