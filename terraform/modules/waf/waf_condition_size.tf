2daf2038e01dafded7347f3f7ea252f4c7a058c9
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2020-11-09 10:46:56 +0000

    Double the allowed size for updating concourse via waf rule

--- terraform/modules/waf/waf_condition_size.tf
+++ terraform/modules/waf/waf_condition_size.tf
@@ -16,7 +16,7 @@ resource "aws_wafregional_size_constraint_set" "size_restrictions" {
   size_constraints {
     text_transformation = "NONE"
     comparison_operator = "GT"
-    size                = "262144"
+    size                = "524288"
 
     field_to_match {
       type = "BODY"
