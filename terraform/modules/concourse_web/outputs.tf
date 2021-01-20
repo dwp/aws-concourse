03b20a9cdd92023f9ee6daeebb4d8c0b32ed80f3
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-01-20 19:16:42 +0000

    fmt
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- terraform/modules/concourse_web/outputs.tf
+++ terraform/modules/concourse_web/outputs.tf
@@ -1,8 +1,8 @@
 output "outputs" {
   value = {
-    http_target_group_arn = aws_lb_target_group.web_http.arn
+    http_target_group_arn     = aws_lb_target_group.web_http.arn
     int_http_target_group_arn = aws_lb_target_group.int_web_http.arn
-    ssh_target_group_arn  = aws_lb_target_group.web_ssh.arn
-    security_group        = aws_security_group.web
+    ssh_target_group_arn      = aws_lb_target_group.web_ssh.arn
+    security_group            = aws_security_group.web
   }
 }
