3e5450ddf916d5a557a92422f4e3213b0a4124c2
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-11-29 17:44:17 +0000

    remove nl
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- terraform/modules/concourse_web/variables.tf
+++ terraform/modules/concourse_web/variables.tf
@@ -110,8 +110,6 @@ variable "s3_prefix_list_id" {
   default = ""
 }
 
-
-
 variable "enterprise_github_certs" {
   type        = list(string)
   description = "A list of certificates that make up the full CA chain that sign the Enterprise GitHub TlS certificates"
