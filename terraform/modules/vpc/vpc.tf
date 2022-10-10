ae726f74ad6124995dbcb0f99111acd6103d51a5
rbryett <13303701+rbryett@users.noreply.github.com>
2022-10-10 16:52:10 +0100

    DW-9274 (#330)
    * DW-9274
    upgrade terraform
    
    * DW-9274 update vpc module version and match AWS rds engine version
    
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source                                   = "dwp/vpc/aws"
-  version                                  = "3.0.14"
+  version                                  = "3.0.15"
   vpc_name                                 = "ci-cd"
   region                                   = data.aws_region.current.name
   vpc_cidr_block                           = var.vpc_cidr_block
