0337cfd39b33b5d722a0a1f4c353e4bfccf8a991
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-06-28 11:17:06 +0100

    fix: mkdir

--- terraform/modules/concourse_web/templates/web_bootstrap.sh
+++ terraform/modules/concourse_web/templates/web_bootstrap.sh
@@ -19,8 +19,8 @@ UUID=$(dbus-uuidgen | cut -c 1-8)
 export HOSTNAME=${name}-$UUID
 
 echo "Setup hcs pre-requisites"
-mkdir /opt/concourse
-mkdir /var/log/concourse
+mkdir -p /opt/concourse
+mkdir -p /var/log/concourse
 chmod u+x /opt/concourse/config_hcs.sh
 /opt/concourse/config_hcs.sh "${hcs_environment}" "${proxy_host}" "${proxy_port}" "${tanium_server_1}" "${tanium_server_2}" "${tanium_env}" "${tanium_port}" "${tanium_log_level}" "${install_tenable}" "${install_trend}" "${install_tanium}" "${tenantid}" "${token}" "${policyid}" "${tenant}"
 
