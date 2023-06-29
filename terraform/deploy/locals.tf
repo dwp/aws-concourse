fd91d824c1ea2106103eed5552a45a15874c8578
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-06-29 10:17:21 +0100

    chore: diable tenable install sue to /opt vol concourse conflict

--- terraform/deploy/locals.tf
+++ terraform/deploy/locals.tf
@@ -58,8 +58,8 @@ locals {
     integration    = "true"
     preprod        = "true"
     production     = "true"
-    management-dev = "true"
-    management     = "true"
+    management-dev = "false" # Conflicting install with Concourse due to necessity to unmount /opt
+    management     = "false" # Conflicting install with Concourse due to necessity to unmount /opt
   }
 
   trend_install = {
